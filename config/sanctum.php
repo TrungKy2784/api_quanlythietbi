7128af63d99bb4fbe679fe71610f76b9b9e99218
NguyenNamTrungKy <trungkynguyen2004@gmail.com>
2026-05-29 15:59:20 +0700

    Hardcode netlify production domain into sanctum stateful array

--- config/sanctum.php
+++ config/sanctum.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1,localhost:3000')),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1,127.0.0.1:8000,localhost:3000,quanlythietbi.netlify.app,api-quanlythietbi.onrender.com')),
 
 
 
