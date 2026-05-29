a04d0145b1a57182cc80435fb37feb910c017195
NguyenNamTrungKy <trungkynguyen2004@gmail.com>
2026-05-29 14:59:42 +0700

    Fix CORS policy for Netlify frontend

--- config/cors.php
+++ config/cors.php
@@ -7,7 +7,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000','https://api-quanlythietbi.onrender.com'], // thêm địa chỉ frontend của bạn tại đây
+    'allowed_origins' => ['http://localhost:3000','https://quanlythietbi.netlify.app'], // thêm địa chỉ frontend của bạn tại đây
 
     'allowed_origins_patterns' => [],
 
